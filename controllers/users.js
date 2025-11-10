e868e1dfbc67d6d41991d91a112eb144de5934e7
ali <ali.hassan.salman.2000@gmail.com>
2025-11-10 15:31:48 +0300

    add role in the user get / route to the response

--- controllers/users.js
+++ controllers/users.js
@@ -7,7 +7,7 @@ const User = require('../models/user');
 router.get('/', async (req, res) => {
   try {
     // Get a list of all users, but only return their username and _id
-    const users = await User.find({}, 'username');
+    const users = await User.find({}, 'username role');
 
     res.json(users);
   } catch (err) {
